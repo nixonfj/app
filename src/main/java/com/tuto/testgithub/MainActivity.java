7953a8e6716425094d65f5b0ecb40fae8ee78257
Nixon-fj <nixon.fernandez@ucr.ac.cr>
2021-09-29 15:10:01 -0600

    Initial commit

---
+++ src/main/java/com/tuto/testgithub/MainActivity.java
@@ -0,0 +1,30 @@
+package com.tuto.testgithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_msj;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_msj =  findViewById(R.id.btn_msj);
+
+        btn_msj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Hola mundo",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }//fin del onCreate
+
+}//fin de la clase
